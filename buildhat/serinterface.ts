ced709a9be436467fcdab42bf98ff3750c1e668f
Ben Rogers <ben@bdr.org>
2022-07-25 18:25:22 +0100

    Tweak checksum

--- buildhat/serinterface.ts
+++ buildhat/serinterface.ts
@@ -339,13 +339,13 @@ export class BuildHAT {
     // """
     let u = 1;
     for (let i = 0; i < data.byteLength; i++) {
-      debug("checksum", i);
+      // debug("checksum", i);
       if ((u & 0x80000000) !== 0) {
         u = (u << 1) ^ 0x1d872b41;
       } else {
         u = u << 1;
       }
-      u = (u ^ data.readInt8(i)) & 0xffffffff;
+      u = (u ^ data.readUInt8(i)) & 0xffffffff;
     }
     return u;
   }
