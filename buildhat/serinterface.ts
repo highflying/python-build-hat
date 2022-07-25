fdd6344410b4f0c6038a764372d6f00eefb68df8
Ben Rogers <ben@bdr.org>
2022-07-25 18:07:26 +0100

    Fix loop

--- buildhat/serinterface.ts
+++ buildhat/serinterface.ts
@@ -335,7 +335,7 @@ export class BuildHAT {
     // :return: Checksum that has been calculated
     // """
     let u = 1;
-    for (let i = 0; i <= data.byteLength; i++) {
+    for (let i = 0; i < data.byteLength; i++) {
       debug("checksum", i);
       if ((u & 0x80000000) !== 0) {
         u = (u << 1) ^ 0x1d872b41;
