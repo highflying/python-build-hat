48598f8da0a7f9c0b7899afdb5fc1a3e937b0f41
Ben Rogers <ben@bdr.org>
2022-07-25 18:38:01 +0100

    Debug

--- buildhat/serinterface.py
+++ buildhat/serinterface.py
@@ -222,6 +222,7 @@ class BuildHAT:
         """
         u = 1
         for i in range(0, len(data)):
+            logging.info(data[i])
             if (u & 0x80000000) != 0:
                 u = (u << 1) ^ 0x1d872b41
             else:
