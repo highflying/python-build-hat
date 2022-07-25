2fe57b4450c8502011e03949f4d42f60bf125ee2
Ben Rogers <ben@bdr.org>
2022-07-25 18:33:57 +0100

    Tweak

--- buildhat/serinterface.ts
+++ buildhat/serinterface.ts
@@ -340,12 +340,14 @@ export class BuildHAT {
     let u = 1;
     for (let i = 0; i <= data.byteLength; i++) {
       // debug("checksum", i);
+      const x = i < data.byteLength ? data.readUInt8(i) : 0;
+
       if ((u & 0x80000000) !== 0) {
         u = (u << 1) ^ 0x1d872b41;
       } else {
         u = u << 1;
       }
-      u = (u ^ data.readUInt8(i)) & 0xffffffff;
+      u = (u ^ x) & 0xffffffff;
     }
     return u;
   }
